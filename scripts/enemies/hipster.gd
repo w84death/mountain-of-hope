262731820a830f86dd8d354ab415139853a90a56
jasam <p.weremczuk@gmail.com>
2015-10-13 21:22:15 +0200

    hipster enemy

--- scripts/enemies/hipster.gd
+++ scripts/enemies/hipster.gd
@@ -2,7 +2,7 @@ extends "res://scripts/enemies/abstract_enemy.gd"
 
 
 func _init(bag).(bag):
-    self.avatar = preload("res://scenes/enemies/spider.xscn").instance()
+    self.avatar = preload("res://scenes/enemies/hipster.xscn").instance()
     self.body_part_head = self.avatar.get_node('body')
     self.body_part_body = self.avatar.get_node('body')
     self.body_part_footer = self.avatar.get_node('body')
