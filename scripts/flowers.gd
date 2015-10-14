82218ae5348fd7dbf14ce05bdfd86fdd8b584b62
Krzysztof Jankowski <kjankowski@ioki.com.pl>
2015-10-14 18:29:27 +0200

    random flower

--- scripts/flowers.gd
+++ scripts/flowers.gd
@@ -1,14 +1,11 @@
 
 extends Sprite
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
 var r
 
 func _ready():
-	#r = rand()*self.get_hframes()
-	#self.set_frame(r);
+	r = randi()*(self.get_hframes()*self.get_vframes())
+	self.set_frame(r);
 	pass
 
 
